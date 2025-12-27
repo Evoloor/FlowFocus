a484c8ac73fdec33bd69b3aa1e7d578c8b99eafc
Evoloor <evoloor@mail.ru>
2025-12-28 01:17:55 +0300

    Стандартный приоритет

---
+++ FlowFocus.Data/Migrations/20251227215908_AddDefaultPriorityIdToSettings.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlowFocus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultPriorityIdToSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DefaultPriorityId",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DefaultPriorityId",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultPriorityId",
+                table: "Settings");
+        }
+    }
+}
