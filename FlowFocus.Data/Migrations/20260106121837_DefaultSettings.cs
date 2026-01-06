fdb57373fb2edb2166c547bd5714bb7a4322719e
Evoloor <evoloor@mail.ru>
2026-01-06 15:19:23 +0300

    Default settings migration

---
+++ FlowFocus.Data/Migrations/20260106121837_DefaultSettings.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FlowFocus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DefaultSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AutoDistributeEnabled", "DefaultPriorityId" },
+                values: new object[] { true, 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "AutoDistributeEnabled", "DefaultPriorityId" },
+                values: new object[] { false, null });
+        }
+    }
+}
